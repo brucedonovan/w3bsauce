2672f01244aad8f0918777068af7b083cd7b0d1f
Bruce Donovan <bruce.donovan@gmail.com>
2025-01-27 12:15:46 +0000

    chore: add script for Meticulous recording in development and preview environments

--- examples/react-app/src/app/layout.tsx
+++ examples/react-app/src/app/layout.tsx
@@ -28,6 +28,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+        {(process.env.NODE_ENV === "development" || process.env.VERCEL_ENV === "preview") && (
+    // eslint-disable-next-line @next/next/no-sync-scripts
+    <script
+      data-recording-token="oxBPQSMtkczoYggcUZEKl7BSQH9HL52uazckvmze"
+      data-is-production-environment="false"
+      src="https://snippet.meticulous.ai/v1/meticulous.js"
+    />
+  )}
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
